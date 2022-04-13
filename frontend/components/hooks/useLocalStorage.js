e31eb9ee1482bf7bfe335ed6fc6134c23bb992e7
AnthonyLayne <kepheus.ds@gmail.com>
2022-04-13 14:14:56 -0400

    setting up useLocalStorage

---
+++ frontend/components/hooks/useLocalStorage.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+//Build a hook called that takes in a key value and an initialValue.
+
+const useLocalStorage = (key, initialValue) => {
+  const [state, setState] = useState();
+};
+
+export default useLocalStorage;
